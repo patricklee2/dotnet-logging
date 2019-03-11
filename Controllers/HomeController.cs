244c8c97bbc7009ae6d9798a834b8e3ac4fdd626
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-03-11 11:42:56 -0700

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿#define TRACE
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_logging.Models;
+
+namespace dotnet_logging.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.Error.WriteLine("requesting Index");
+            Console.WriteLine("requesting Index");
+            Trace.WriteLine("requesting Index");
+            Trace.TraceError("requesting Index");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            Console.Error.WriteLine("requesting Privacy");
+            Console.WriteLine("requesting Privacy");
+            Trace.WriteLine("requesting Privacy");
+            Trace.TraceError("requesting Privacy");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace dotnet_logging
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            Trace.AutoFlush = true;
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
